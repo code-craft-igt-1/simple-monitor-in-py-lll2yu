368643cde3969d0dd0d5cec5175c4987173ef590
Gagandeep Singh <Gagandeep.Singh@philips.com>
2024-09-01 12:08:27 +0530

    reduce complexity and add more tests
    split the vital check function in multiple functions.
    add more test cases.
    PEP8 W503 is causing problems(if fixed, causes W504), add to flake8 ignore.

--- monitor.py
+++ monitor.py
@@ -1,37 +1,46 @@
-
 from time import sleep
 import sys
 
 
-def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+def print_critical_message(message, seconds_to_showmessage):
+    print(f"\n{message}")
+    full_cycles = seconds_to_showmessage // 2
+    remaining_seconds = seconds_to_showmessage % 2
+    for _ in range(full_cycles):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
+
+    if remaining_seconds:
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+
+
+def check_vital(vital_name, vital_value,
+                lower_limit_for_vital, upper_limit_for_vital):
+    if (
+        vital_value < lower_limit_for_vital
+        or vital_value > upper_limit_for_vital
+    ):
+        messages = {
+            'Temperature': 'Temperature critical!',
+            'Pulse Rate': 'Pulse Rate is out of range!',
+            'Oxygen Saturation': 'Oxygen Saturation out of range!'
+        }
+
+        message = messages.get(vital_name, f'{vital_name} is out of range!')
+        print_critical_message(message, 12)
+        return False
+    return True
+
+
+def vitals_ok(temperature_inF, pulse_rate, spo2):
+    return all([
+        check_vital('Temperature', temperature_inF, 95, 102),
+        check_vital('Pulse Rate', pulse_rate, 60, 100),
+        check_vital('Oxygen Saturation', spo2, 90, 100)
+    ])
